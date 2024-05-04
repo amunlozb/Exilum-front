dbe0db5841d995d2fb9cb4248e35807bdb7bfeed
amunlozb <amunlozb@g.educaand.es>
2024-05-04 14:40:08 +0200

    fixed isAdmin to new endpoint

--- src/hooks/isAdmin.js
+++ src/hooks/isAdmin.js
@@ -11,7 +11,8 @@ export default async function isAdmin() {
 
     const token = await user.getIdToken();
 
-    const response = await fetch('http://localhost:8080/api/getAuthorities', {
+    // @TODO: change when deployed
+    const response = await fetch('http://localhost:8080/api/getRoles', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
