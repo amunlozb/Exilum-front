d50e2c6c1798839550bf433ff5c45c52125b9533
amunlozb <amunlozb@g.educaand.es>
2024-05-07 17:47:12 +0200

    added responsiveness to Header

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { DarkThemeToggle } from "flowbite-react";
+import { Navbar, DarkThemeToggle } from "flowbite-react";
+
+// TODO: center collapsable items to center
 
 function Header() {
   const [top, setTop] = useState(true);
@@ -13,18 +15,16 @@ function Header() {
     window.addEventListener("scroll", scrollHandler);
     return () => window.removeEventListener("scroll", scrollHandler);
   }, [top]);
+
   return (
     <header
       className={`fixed w-full z-30 md:bg-opacity-90 dark:bg-opacity-95 transition duration-300 ease-in-out ${
-        !top && "bg-white dark:bg-gray-900 backdrop-blur-sm shadow-lg dark:shadow-gray-950"
+        !top && "bg-white dark:bg-gray-900 backdrop-blur-sm shadow-lg dark:shadow-black"
       }`}
     >
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
-          {/* Site branding */}
-          <div className="flex-shrink-0 mr-4">
-            {/* Logo */}
-            <Link to="/" className="block" aria-label="Link to X">
+        <Navbar rounded className="dark:bg-gray-900">
+            <Navbar.Brand as={Link} to="/">
               <svg
                 className="w-8 h-8"
                 viewBox="0 0 32 32"
@@ -52,57 +52,49 @@ function Header() {
                   fillRule="nonzero"
                 />
               </svg>
-            </Link>
-          </div>
+            </Navbar.Brand>
 
-          {/* Site navigation */}
-          <nav className="flex flex-grow">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
-              <li>
-                {/* Remove investments from here (should only be accessible when logged in) */}
-              <Link
-                  to="/investments"
-                  className="font-medium text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500
-                px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  Investments
-                </Link>
-                </li>
-                {/*  */}
-                <li>
-                <Link
-                  to="/signin"
-                  className="font-medium text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500
-                px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >
-                  Sign in
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/signup"
-                  className="btn-sm text-gray-200 dark:hover:text-white bg-gray-900 hover:bg-gray-800 ml-3"
-                >
-                  <span>Sign up</span>
-                  <svg
-                    className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
+            <Navbar.Toggle />
+          <Navbar.Collapse>
+          
+                  <Navbar.Link 
+                    href="/investments"
+                    className=" md:text-lg text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500
+                    md:px-5 md:py-3 transition duration-150 ease-in-out "
                   >
-                    <path
-                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
-                      fillRule="nonzero"
-                    />
-                  </svg>
-                </Link>
-              </li>
+                    Investments
+                  </Navbar.Link>
 
-              <li className="px-3">
-                <DarkThemeToggle className="focus:ring-0" />
-              </li>
-            </ul>
-          </nav>
-        </div>
+                  <Navbar.Link
+                    href="/signin"
+                    className="md:text-lg text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500
+                    md:px-5 md:py-3 transition duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </Navbar.Link>
+
+                  <Navbar.Link
+                    href="/signup"
+                    className="md:text-lg text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-500
+                    md:px-5 md:py-3 transition duration-150 ease-in-out flex items-center"
+                  >
+                    Sign up
+                    <svg
+                      className="w-3 h-3 fill-current text-gray-800 dark:text-gray-300 flex-shrink-0 ml-2 -mr-1"
+                      viewBox="0 0 12 12"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                        fillRule="nonzero"
+                      />
+                    </svg>
+                  </Navbar.Link>
+                <li className="px-3">
+                </li>
+        <DarkThemeToggle className="focus:ring-0"/>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     </header>
   );
