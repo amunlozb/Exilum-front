ba12b6f171c1a75491bc73d554e3880e0aba6183
amunlozb <amunlozb@g.educaand.es>
2024-06-06 15:28:34 +0200

    changed api root url to deployed GCP url

--- src/const/root_url.js
+++ src/const/root_url.js
@@ -1,3 +1,4 @@
-const root_url = "http://localhost:8080";
-// "https://exilum-back-c24f5etkvq-ey.a.run.app";
+const root_url = 
+// "http://localhost:8080";
+"https://exilum-back-c24f5etkvq-ey.a.run.app";
 export default root_url;
